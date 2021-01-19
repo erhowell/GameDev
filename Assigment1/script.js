67e0344952ab5617acd7c359352d617f5397281e
Liz Howell <erhowell15@gmail.com>
2021-01-19 14:33:01 -0700

    small bugs fixed

--- Assigment1/script.js
+++ Assigment1/script.js
@@ -15,28 +15,32 @@ class UserEvent {
         }
     }
     textOutput() {
-        let output = '<br />Event: '+ this.name +' '+this.times+' Times Remaining';
+        let output = 'Event: '+ this.name +' '+this.times+' Times Remaining.<br />';
         return output;
     } 
   }
 
 
   function update(time){ 
-    allEvents = allEvents.map(x => {x.update(time); return x;})
-                .sort((e1,e2) => {return e2.timebetween- e1.timebetween;});
-    
-    allEvents.forEach(e => {
-        if(e.lastActive == time)
-        {
-            render(e);
-        }
-    });
-    allEvents = allEvents.filter(a => a.times > 0);
-}
+      // accepts the elapsed time as a parameter.  It is in the update function where any active events are updated.
+        allEvents = allEvents.map(x => {x.update(time); return x;})
+                    .sort((e1,e2) => {return e2.timebetween- e1.timebetween;});
+        
+        allEvents.forEach(e => {
+            if(e.lastActive == time)
+            {
+                render(e);
+            }
+        });
+        allEvents = allEvents.filter(a => a.times > 0);
+    }
 
 function render(event){
     // It is in the render function where any events that need reporting are displayed.
-    document.getElementById("outputText").innerHTML += event.textOutput();
+    
+    let node = document.getElementById("outputText")
+    node.innerHTML = event.textOutput() + node.innerHTML;
+    node.scrollTop = node.scrollHeight;
 }
 
 function submitClicked()
