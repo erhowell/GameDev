5daa9bd07d4bdf310db47266f35e03c31134861a
Liz Howell <erhowell15@gmail.com>
2021-01-19 14:06:25 -0700

    logic updated

--- Assigment1/script.js
+++ Assigment1/script.js
@@ -1,15 +1,58 @@
+
+let allEvents = [];
+class UserEvent {
+    constructor(name, interval, times) {
+      this.name = name;
+      this.interval = interval;
+      this.times = times;
+      this.lastActive = 0;
+    }
+    update(currentTime){
+        this.timebetween = currentTime - this.lastActive;
+        if((this.lastActive == 0) || (this.timebetween >= this.interval)){
+            this.lastActive = currentTime;
+            this.times -= 1;
+        }
+    }
+    textOutput() {
+        let output = '<br />Event: '+ this.name +' '+this.times+' Times Remaining';
+        return output;
+    } 
+  }
 function gameLoop () {
      // in which you update the elapsed time since the last time the function was called
-     let soMany = 10;
-     document.getElementById("outputText").innerHTML += "\nEvent:`This is ${soMany} times easier!`"
-  }
-
+        if(allEvents.length > 0)
+        {
+            update(performance.now());
+        }
+        window.requestAnimationFrame(gameLoop);
+    }
 function update(time){ 
-    //accepts the elapsed time as a parameter. Active events are updated.
-
+    allEvents = allEvents.map(x => {x.update(time); return x;})
+                .sort((e1,e2) => {return e2.timebetween- e1.timebetween;});
+    
+    allEvents.forEach(e => {
+        if(e.lastActive == time)
+        {
+            render(e);
+        }
+    });
+    allEvents = allEvents.filter(a => a.times > 0);
 }
 
-function render(){
+function render(event){
     // It is in the render function where any events that need reporting are displayed.
+ 
+    document.getElementById("outputText").innerHTML += event.textOutput();
+}
 
+function submitClicked()
+{
+    console.log('submitClicked');
+    let eventName = document.getElementById("inputEvent").value;
+    let interval = document.getElementById("inputInterval").value;
+    let times = document.getElementById("inputTimes").value;
+    
+    allEvents.push(new UserEvent(eventName, interval, times));
+    gameLoop()
 }
