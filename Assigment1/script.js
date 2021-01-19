6162051492fa89497464ff1da2bbf3cddb79a7a9
Liz Howell <erhowell15@gmail.com>
2021-01-19 12:25:05 -0700

    front end complete

---
+++ Assigment1/script.js
@@ -0,0 +1,15 @@
+function gameLoop () {
+     // in which you update the elapsed time since the last time the function was called
+     let soMany = 10;
+     document.getElementById("outputText").innerHTML += "\nEvent:`This is ${soMany} times easier!`"
+  }
+
+function update(time){ 
+    //accepts the elapsed time as a parameter. Active events are updated.
+
+}
+
+function render(){
+    // It is in the render function where any events that need reporting are displayed.
+
+}
